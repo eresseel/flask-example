0ba63bdea65a9f8b3ccbd374cd3e592a0c1a8d0b
lberes <beres.laszlo1990@gmail.com>
2023-04-05 11:19:03 +0200

    Add redis code

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from multiprocessing import Value
+from dotenv import load_dotenv
 import subprocess
+import redis
+import os
 
 app = Flask(__name__)
+load_dotenv()
+r = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), \
+    password=os.environ["REDIS_PASSWORD"])
 counter = Value('i', 0)
  
 def get_git_revision_short_hash():
@@ -21,11 +27,17 @@ def alert(alert=None):
     with counter.get_lock():
         counter.value += 1
         out = counter.value
+    r.mset({"counter": out})
     return ""
 
 @app.route('/counter')
 def count(alert=None):
-    return render_template('counter.html', alert=counter.value)
+    if r.exists("runner"):
+        out = r.get("counter").decode("utf-8")
+    else:
+        out = counter.value
+
+    return render_template('counter.html', alert=out)
 
 @app.route('/version')
 def version(git_sort_hash=None):
